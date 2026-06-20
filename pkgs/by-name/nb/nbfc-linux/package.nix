26fb90f135f3e7205b3bff3e829703c65b388141
anubis <BK_anubis_GG@proton.me>
2026-06-20 18:21:49 +0100

    lets see if this works

--- pkgs/by-name/nb/nbfc-linux/package.nix
+++ pkgs/by-name/nb/nbfc-linux/package.nix
@@ -23,18 +23,17 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   nativeBuildInputs = [
-    autoconf
-    automake
+    autoreconfHook
     pkg-config
+  ];
+
+  buildInputs = [
     lua5_4
     curl
     libxml2
     openssl
   ];
 
-  preConfigure = ''
-    ./autogen.sh
-  '';
   configureFlags = [
     "--bindir=${placeholder "out"}/bin"
   ];
@@ -45,7 +44,7 @@ stdenv.mkDerivation (finalAttrs: {
       nbfc-linux provides fan control service for notebooks
     '';
     homepage = "https://github.com/nbfc-linux/nbfc-linux";
-    license = lib.licenses.gpl3;
+    license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [
       Celibistrial
       bohanubis
